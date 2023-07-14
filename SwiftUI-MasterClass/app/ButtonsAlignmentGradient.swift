b0bf00946cada06c66a2a62bbf8e6c25591a5959
Salvador Vilca <talktosalvador@gmail.com>
2023-07-14 18:35:08 -0400

    added corner buttons

--- SwiftUI-MasterClass/app/ButtonsAlignmentGradient.swift
+++ SwiftUI-MasterClass/app/ButtonsAlignmentGradient.swift
@@ -4,6 +4,12 @@ struct ButtonsAlignmentGradient: View {
     private let timer = Timer.publish(every: 1, on: .main, in: .default).autoconnect()
     private let colors = [Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), Color(#colorLiteral(red: 0.7113229036, green: 0.4679040909, blue: 0.9639104009, alpha: 1)), Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)), Color(#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)), Color(#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)), Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)), Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))]
     private let url: String = "https://cdn-icons-png.flaticon.com/128/6941/6941697.png"
+    private let cornerButtons = [
+        CornerButton(name: "w", align: .topLeading),
+        CornerButton(name: "x", align: .topTrailing),
+        CornerButton(name: "y", align: .bottomLeading),
+        CornerButton(name: "z", align: .bottomTrailing)
+    ]
     @State private var start = UnitPoint(x: 0, y: 0)
     @State private var end = UnitPoint(x: 1, y: 1)
     @State var startAngle = 0
@@ -14,8 +20,10 @@ struct ButtonsAlignmentGradient: View {
         ZStack {
             animatedLinearGradientBackground
                 .ignoresSafeArea()
-            
+
             centralButton
+
+            alignedButtons
         }
     }
 
@@ -50,7 +58,7 @@ struct ButtonsAlignmentGradient: View {
         })
         .blur(radius: 5)
     }
-    
+
     // MARK: IMAGES
 
     var localImage: some View {
@@ -126,6 +134,26 @@ struct ButtonsAlignmentGradient: View {
         }
         .padding(40)
     }
+
+    // MARK: CORNER BUTTONS
+
+    var alignedButtons: some View {
+        ForEach(cornerButtons) { button in
+            Button { print(button.name) } label: {
+                Text(button.name)
+                    .padding()
+            }
+            .border(.black)
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: button.align)
+        }
+    }
+}
+
+struct CornerButton: Identifiable {
+    let id = UUID()
+    let name: String
+    let align: Alignment
 }
 
 struct ButtonsAlignmentGradient_Previews: PreviewProvider {
