dadc95bfed429bb21f15257658e6835867588917
Salvador Vilca <talktosalvador@gmail.com>
2023-07-14 18:32:12 -0400

    added central button with local and cloud image

--- SwiftUI-MasterClass/app/ButtonsAlignmentGradient.swift
+++ SwiftUI-MasterClass/app/ButtonsAlignmentGradient.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ButtonsAlignmentGradient: View {
     private let timer = Timer.publish(every: 1, on: .main, in: .default).autoconnect()
     private let colors = [Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), Color(#colorLiteral(red: 0.7113229036, green: 0.4679040909, blue: 0.9639104009, alpha: 1)), Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)), Color(#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)), Color(#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)), Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)), Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))]
+    private let url: String = "https://cdn-icons-png.flaticon.com/128/6941/6941697.png"
     @State private var start = UnitPoint(x: 0, y: 0)
     @State private var end = UnitPoint(x: 1, y: 1)
     @State var startAngle = 0
@@ -13,6 +14,8 @@ struct ButtonsAlignmentGradient: View {
         ZStack {
             animatedLinearGradientBackground
                 .ignoresSafeArea()
+            
+            centralButton
         }
     }
 
@@ -46,6 +49,82 @@ struct ButtonsAlignmentGradient: View {
             endAngle += 20
         })
         .blur(radius: 5)
+    }
+    
+    // MARK: IMAGES
+
+    var localImage: some View {
+        Image("crown")
+            .fitImage()
+    }
+
+    var simpleCloudImage: some View {
+        AsyncImage(url: URL(string: url))
+    }
+
+    var scaledCloudImage: some View {
+        AsyncImage(url: URL(string: url), scale: 0.5)
+    }
+
+    var placeholderCloudImage: some View {
+        AsyncImage(url: URL(string: url)) { image in
+            image
+                .fitImage()
+        } placeholder: {
+            Image(systemName: "photo.circle.fill")
+                .fitIcon()
+        }
+    }
+
+    var phaseCloudImage: some View {
+        AsyncImage(
+            url: URL(string: url),
+            content: { phase in
+                if let image = phase.image {
+                    image
+                        .fitImage()
+                } else if phase.error != nil {
+                    Image(systemName: "ant.circle.fill")
+                        .fitIcon()
+                } else {
+                    Image(systemName: "photo.circle.fill")
+                        .fitIcon()
+                }
+            }
+        )
+    }
+
+    var animatedPhaseCloudImage: some View {
+        AsyncImage(
+            url: URL(string: url),
+            transaction: Transaction(animation: .spring())
+        ) { phase in
+            switch phase {
+            case .success(let image):
+                image
+                    .fitImage()
+//                    .transition(.move(edge: .bottom))
+//                    .transition(.slide)
+                    .transition(.scale)
+            case .failure:
+                Image(systemName: "ant.circle.fill")
+                    .fitIcon()
+            case .empty:
+                Image(systemName: "photo.circle.fill")
+                    .fitIcon()
+            @unknown default:
+                ProgressView()
+            }
+        }
+    }
+
+    // MARK: CENTRAL BUTTON
+
+    var centralButton: some View {
+        Button { print("central") } label: {
+            animatedPhaseCloudImage
+        }
+        .padding(40)
     }
 }
 
