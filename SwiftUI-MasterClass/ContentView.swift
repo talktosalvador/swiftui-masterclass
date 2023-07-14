a4f31fec52e9acedf9e992a4ca66a46cd23e74d6
Salvador Vilca <talktosalvador@gmail.com>
2023-07-14 18:16:29 -0400

    Initial Commit

---
+++ SwiftUI-MasterClass/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-MasterClass
+//
+//  Created by Salvador on 14/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-MasterClass/SwiftUI_MasterClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MasterClassApp.swift
+//  SwiftUI-MasterClass
+//
+//  Created by Salvador on 14/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MasterClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
